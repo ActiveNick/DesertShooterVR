642b7681e3c4fcbb96a641bb53ca081882397dd4
Nick Landry <nlandry@activenick.net>
2016-10-09 18:43:41 -0400

    Added spatial sound effects

---
+++ Assets/Scripts/StarshipSoundManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarshipSoundManager : MonoBehaviour {
+
+    public AudioSource shipAudio;
+    public AudioClip landingClip;
+    public AudioClip thudClip;
+    public AudioClip warpClip;
+
+    private float originalVolume;
+
+	// Use this for initialization
+	void Start () {
+        originalVolume = shipAudio.volume;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void PlayLandingSound()
+    {
+        // Change the clip to the firing clip and play it.
+        shipAudio.volume = originalVolume;
+        shipAudio.clip = landingClip;
+        shipAudio.Play();
+    }
+
+    private void PlayThudSound()
+    {
+        // Change the clip to the firing clip and play it.
+        shipAudio.volume = 1;
+        shipAudio.clip = thudClip;
+        shipAudio.Play();
+    }
+
+    private void PlayWarpSound()
+    {
+        // Change the clip to the firing clip and play it.
+        shipAudio.volume = 1;
+        shipAudio.clip = warpClip;
+        shipAudio.Play();
+    }
+}
