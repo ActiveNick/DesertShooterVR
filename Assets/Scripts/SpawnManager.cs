f0b43afd064c7fea7d9cea76324ff422d4946656
Nick Landry <nick@mobility42.com>
2017-06-22 13:51:04 -0400

    Added regular spawning of new enemies after the ship lands.

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject Enemy;
+    public GameObject EnemyTarget;
+    public float SpawnInterval = 5;
+
+    //private GameObject enemies[];
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("CreateEnemy", 12f, SpawnInterval);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void CreateEnemy()
+    {
+        Vector3 spawnpoint = new Vector3(-40, 0, -36);
+
+        GameObject newEnemy = (GameObject)Instantiate(Enemy, spawnpoint, Quaternion.identity);
+        newEnemy.GetComponent<EnemyMovement>().DesiredTarget = EnemyTarget;
+    }
+}
