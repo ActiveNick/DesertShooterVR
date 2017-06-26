ace86a0780d61f01ab4374f0823f54f973122eed
Nick Landry <nick@mobility42.com>
2017-06-25 20:25:56 -0400

    Started new projectile shooter implementation using raycasting - work in progress

--- Assets/Complete/Scripts/Shell/ShellExplosion.cs
+++ Assets/Complete/Scripts/Shell/ShellExplosion.cs
@@ -5,43 +5,20 @@ namespace Complete
 {
     public class ShellExplosion : MonoBehaviour
     {
-        public LayerMask m_TankMask;                        // Used to filter what the explosion affects, this should be set to "Players".
+
         public ParticleSystem m_ExplosionParticles;         // Reference to the particles that will play on bullet impact explosion.
         public ParticleSystem m_DestroyedParticles;         // Reference to the particles that will play when an enemy id destroyed
         public AudioSource m_ExplosionAudio;                // Reference to the audio that will play on explosion.
         public AudioClip m_ShellHitClip;
         public AudioClip m_EnemyDestroyedClip;
-        //public float m_MaxDamage = 100f;                    // The amount of damage done if the explosion is centred on a tank.
-        //public float m_ExplosionForce = 1000f;              // The amount of force added to a tank at the centre of the explosion.
-        public float m_MaxLifeTime = 2f;                    // The time in seconds before the shell is removed.
-        //public float m_ExplosionRadius = 0.5f;                // The maximum distance away from the explosion tanks can be and are still affected.
 
+        public float m_MaxLifeTime = 2f;                    // The time in seconds before the shell is removed.
 
         private void Start ()
         {
             // If it isn't destroyed by then, destroy the shell after it's lifetime.
             Destroy (gameObject, m_MaxLifeTime);
         }
-
-        //private float CalculateDamage (Vector3 targetPosition)
-        //{
-        //    // Create a vector from the shell to the target.
-        //    Vector3 explosionToTarget = targetPosition - transform.position;
-
-        //    // Calculate the distance from the shell to the target.
-        //    float explosionDistance = explosionToTarget.magnitude;
-
-        //    // Calculate the proportion of the maximum distance (the explosionRadius) the target is away.
-        //    float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
-
-        //    // Calculate damage as this proportion of the maximum possible damage.
-        //    float damage = relativeDistance * m_MaxDamage;
-
-        //    // Make sure that the minimum damage is always 0.
-        //    damage = Mathf.Max (0f, damage);
-
-        //    return damage;
-        //}
 
         private void OnCollisionEnter(Collision otherObj)
         {
